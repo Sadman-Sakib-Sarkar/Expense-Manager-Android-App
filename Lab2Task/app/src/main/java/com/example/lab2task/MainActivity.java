a3338119168bb416ef93275cf9148df660e10151
Sadman Sakib Sarkar <jucse29.391@gmail.com>
2024-10-28 20:11:05 +0600

    uploading to github

--- Lab2Task/app/src/main/java/com/example/lab2task/MainActivity.java
+++ Lab2Task/app/src/main/java/com/example/lab2task/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lab2task;
 
 import android.os.Bundle;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,38 +19,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    public class Phones{
-        List<String>getInfo(String group){
-            List<String>info= new ArrayList<>();
-            if (group.equals("Samsung")) {
-                info.add("\nSamsung Galaxy S21");
-                info.add("\nSamsung Galaxy S21 Plus");
-                info.add("\nSamsung Galaxy S21 Ultra");
-                info.add("\nSamsung Galaxy S24");
-                info.add("\nSamsung Galaxy S24 Plus");
-                info.add("\nSamsung Galaxy S24 Ultra");
-            }
-
-            if (group.equals("Apple")) {
-                info.add("\nIphone 14");
-                info.add("\nIphone 14 Pro");
-                info.add("\nIphone 14 Pro Max");
-                info.add("\nIphone 15");
-                info.add("\nIphone 15 Pro");
-                info.add("\nIphone 15 Pro Max");
-            }
-
-            if (group.equals("Xiaomi")) {
-                info.add("\nRedmi 13");
-                info.add("\nRedmi Note 13 Pro");
-                info.add("\nRedmi Note 13 pro 5g");
-                info.add("\nRedmi Note 13 pro plus 5g");
-            }
-
-            return info;
-        }
-    }
-    private Phones expert= new Phones();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,13 +36,8 @@ public class MainActivity extends AppCompatActivity {
         TextView info2 =(TextView)  findViewById(R.id.infoss);
         Spinner dis = (Spinner) findViewById(R.id.spinner);
         String type=String.valueOf(dis.getSelectedItem());
-        List<String>infoList =expert.getInfo(type);
-        StringBuilder infoFormatted = new StringBuilder();
-        for(String inf : infoList)
-        {
-            infoFormatted.append(inf).append('\n');
-        }
-        info2.setText("Available Models");
-        info.setText(infoFormatted);
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(SecondActivity.EXTRA_MESSAGE, type);
+        startActivity(intent);
     }
 }
--- StudentId/app/src/main/java/com/example/studentid/MainActivity.java
+++ StudentId/app/src/main/java/com/example/studentid/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.studentid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageView facebook = findViewById(R.id.facebook);
+        facebook.setOnClickListener(v -> openLink("https://www.facebook.com/Sadman.Sakib.Sarkar"));
+
+        ImageView instagram = findViewById(R.id.instagram);
+        instagram.setOnClickListener(v -> openLink("https://www.instagram.com/_.s_a_d_m_a_n._/"));
+
+        ImageView linkedin = findViewById(R.id.linkedin);
+        linkedin.setOnClickListener(v -> openLink("https://www.linkedin.com/in/sadman-sakib-9785a92b7"));
+
+        ImageView github = findViewById(R.id.github);
+        github.setOnClickListener(v -> openLink("https://github.com/Sadman-Sakib-Sarkar"));
+
+        ImageView mypic = findViewById(R.id.mypic);
+        mypic.setOnClickListener(v -> openLink("https://sadmansakib.netlify.app/"));
     }
+
+    private void openLink(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
 }
